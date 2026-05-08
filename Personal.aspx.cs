b3be1dbaa95521f56879ac97d340607edc4bd88c
Luis H. Marin <luchiitoz@gmail.com>
2026-05-08 15:17:04 -0500

    ReporteSeguimientosElementoConfiguracion mostrando datos

--- Personal.aspx.cs
+++ Personal.aspx.cs
@@ -103,8 +103,8 @@ public partial class Personal : System.Web.UI.Page
         Cargar_Datos(this.Cargo, "[dbo].[pr_Obtener_Cargos]", "");
         if (this.__mensaje.Value.ToString().Trim() != "") { return; }
         // Cargar Locales y Areas sin filtro para que esten disponibles en el formulario
-        Cargar_Datos(this.Local, "[dbo].[pr_Obtener_Locales]", "", 0);
-        Cargar_Datos(this.Area, "[dbo].[pr_Obtener_Areas]", "", 0);
+        Cargar_Datos(this.Local, "[dbo].[pr_Obtener_Locales]", "");
+        Cargar_Datos(this.Area, "[dbo].[pr_Obtener_Areas]", "");
         Cargar_Lista_Personal();
     }
 
